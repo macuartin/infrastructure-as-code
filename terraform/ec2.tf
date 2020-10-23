413a8f41a0eb7a52071a58216777ec0b78267c90
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-10-23 15:09:05 -0500

    nueva maquina virtual

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -11,3 +11,18 @@ resource "aws_instance" "jks_master_ec2" {
       Name = "${var.stack_id}-jks-instance"
     }
 }
+
+
+resource "aws_instance" "jks_master_ec2_miguel" { 
+  ami = data.aws_ami.jenkins_image.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public_subnet_primary.id
+  vpc_security_group_ids = [aws_security_group.sg_ssh_allow.id]
+
+  key_name = "macuartin_key_pair"
+
+    tags = {
+      Name = "${var.stack_id}-jks-instance"
+    }
+}
